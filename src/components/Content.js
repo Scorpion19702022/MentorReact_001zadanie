dc306a68fba44b32cba9fd1b9995ffd75d0cc3c8
skorpion <jmosinski@o2.pl>
2023-10-14 19:57:19 +0200

    corected app and finish app

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Content = props => {
+	const text =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nulla corporis porro rem error voluptatibus, aperiam praesentium voluptatem quo tempore rerum adipisci deserunt minima quibusdam inventore tempora necessitatibus omnis nemo a natus voluptate animi odio quidem officiis! Animi eligendi dolore aspernatur odio distinctio inventore necessitatibus placeat. Optio placeat officiis quas.'
+
 	return (
 		<div className={props.view ? 'viewcontent' : 'noviewcontent'}>
-			<p className='textcontent'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit corporis optio et laboriosam minima nulla iusto
-				deleniti voluptatem. Molestiae, voluptates! Labore, facilis sequi accusantium dolorum illum porro, quos iure
-				odit ipsam repellendus exercitationem enim aspernatur, at fugiat? Excepturi, voluptatem labore.
-			</p>
+			<p className='textcontent'>{text}</p>
 		</div>
 	)
 }
